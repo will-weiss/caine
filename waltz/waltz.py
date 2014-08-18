ee8df8615004113443d20ee5609219da33bf937f
will-weiss <will.weiss1230@gmail.com>
2014-08-17 23:04:56 -0400

    adds some documentation

--- waltz/waltz.py
+++ waltz/waltz.py
@@ -4,6 +4,9 @@ import time
 from gevent.timeout import Timeout
 
 class SupportingActor(object):
+    """
+    concurrently processes items in its inbox 
+    """
     def __init__(self, timeout = 10, **kwargs):
         self.inbox = Queue()
         self.timeout = timeout
@@ -11,18 +14,30 @@ class SupportingActor(object):
 
     @staticmethod
     def receive(message):
+        """
+        method to call using messages from inbox
+        """
         raise NotImplemented()
 
     @staticmethod
     def callback():
-        print "No more tasks in queue."
+        """
+        method to call when inbox is empty
+        """
+        print "No more messages in inbox."
 
     @staticmethod
     def handle():
+        """
+        method to call when operation fails
+        """
         print "Operation failed."
 
     @staticmethod
     def listen(inbox, receive, callback, timeout, handle, _raise_timeout):
+        """
+        listens for incoming messages
+        """
         try:
             SupportingActor._listen(inbox, timeout, receive, _raise_timeout)
         except Timeout:
@@ -37,16 +52,19 @@ class SupportingActor(object):
     @staticmethod
     def _listen(inbox, timeout, receive, _raise_timeout):
         use_timeout = True if type(timeout) == int else False
-        if use_timeout:
+        if use_timeout: # Set alarm to raise a timeout after timeout seconds
             signal.signal(signal.SIGALRM, _raise_timeout)
             signal.alarm(timeout)
         running = True
         while running:
             message = inbox.get()
-            if use_timeout: signal.alarm(0)
+            if use_timeout: signal.alarm(0) # Alarm turned off when processing message
             receive(message)
-            if use_timeout: signal.alarm(timeout)
+            if use_timeout: signal.alarm(timeout) # Alarm reset
             
     def __call__(self):
+        """
+        begin processing inbox
+        """
         self.process = Process(target = SupportingActor.listen, args = [self.inbox, self.receive, self.callback, self.timeout, self.handle, self._raise_timeout])
         self.process.start()

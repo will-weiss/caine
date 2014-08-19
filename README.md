dcc6d52c22481538c363f9ba68e0f17fe6a1b4d8
will-cognius <will@cognius.com>
2014-08-19 12:11:40 -0400

    Adds SupportingCast class

--- README.md
+++ README.md
@@ -14,12 +14,12 @@ supporting actors offer concurrent inbox processing
 def print_square(message):
     print "%s squared is: %s" %(message, message**2)
 
-square_printer = SupportingActor(receive = print_square, timeout = 5)
+square_printing_actor = SupportingActor(receive = print_square, timeout = 5)
 
-square_printer()
+square_printing_actor() # Call an instance of SupportingActor to commence receiving messages
 
 for i in xrange(1,11):
-    square_printer.inbox.put(i)
+    square_printing_actor.inbox.put(i)
 
 ### Output
 1 squared is: 1
@@ -32,4 +32,33 @@ for i in xrange(1,11):
 8 squared is: 64
 9 squared is: 81
 10 squared is: 100
+No more messages in inbox.</code></pre>
+
+### More Horsepower
+
+<pre><code>from waltz import SupportingCast
+
+def print_square(message):
+    import random
+    time.sleep(random.randint(0,4))
+    print "%s squared is: %s" %(message, message**2)
+
+square_printing_cast = SupportingCast(receive = print_square, num = 3, timeout = 5)
+
+square_printing_cast() # Call an instance of SupportingCast to have its actors commence receiving messages
+
+for i in xrange(1,11):
+    square_printing_cast.inbox.put(i)
+
+### Output
+2 squared is: 4
+1 squared is: 1
+7 squared is: 49
+8 squared is: 64
+10 squared is: 100
+3 squared is: 9
+4 squared is: 16
+5 squared is: 25
+6 squared is: 36
+9 squared is: 81
 No more messages in inbox.</code></pre>
--- waltz/waltz.py
+++ waltz/waltz.py
@@ -1,15 +1,22 @@
-from multiprocessing import Process, Queue, Lock
+import multiprocessing
+from multiprocessing import Process, Queue, Lock, Pipe, Value
 import signal
 import time
 from gevent.timeout import Timeout
 
 class SupportingActor(object):
     """
-    concurrently processes items in its inbox 
+    Data structure with operations for processing objects in its inbox.
+
+    Parameters
+    __________
+    timeout : int or None, default None
+        Number of seconds between message receptions before callback is executed
+    kwargs : object
+
     """
-    def __init__(self, timeout = 10, **kwargs):
+    def __init__(self, timeout = None, **kwargs):
         self.inbox = Queue()
-        self.lock = Lock()
         self.timeout = timeout
         for nm, val in kwargs.iteritems(): setattr(self, nm, val)
 
@@ -28,116 +35,105 @@ class SupportingActor(object):
         print "No more messages in inbox."
 
     @staticmethod
-    def handle(lock):
+    def handle(exc):
         """
-        method to call when operation fails
+        method to call when non-timeout exception raised by listen
         """
-        lock.acquire()
-        print "Operation failed."
+        raise exc
 
     @staticmethod
-    def listen(inbox, receive, callback, timeout, handle, lock):
+    def listen(inbox, receive, callback, timeout, handle):
         """
-        listens for incoming messages
+        listens for incoming messages, executes callback after Timeout, 
         """
         try:
-            SupportingActor._listen(inbox, timeout, receive, lock)
+            SupportingActor._listen(inbox, timeout, receive)
         except Timeout:
             callback()
-        except not Timeout:
-            handle(lock)
+        except Exception as exc:
+            handle(exc)
 
     @staticmethod
-    def _listen(inbox, timeout, receive, lock):
-        use_timeout = True if type(timeout) == int else False
-        if use_timeout: signal.signal(signal.SIGALRM, _raise_timeout) # _raise_timeout is called when alarm goes off
+    def _listen(inbox, timeout, receive):
+        use_timeout = True if type(timeout) == int else False           # We use a timeout if timeout is an integer, otherwise not
+        if use_timeout: signal.signal(signal.SIGALRM, _raise_timeout)   # _raise_timeout is called when alarm goes off
         running = True
         while running:
-            if use_timeout: signal.alarm(timeout) # Set/Reset alarm
-            lock.acquire()
-            lock.release()
+            if use_timeout: signal.alarm(timeout)   # Set/Reset alarm
             message = inbox.get()
-            if use_timeout: signal.alarm(0) # Alarm turned off when processing message
+            if use_timeout: signal.alarm(0)         # Alarm turned off when processing message
             receive(message)
 
     def __call__(self):
         """
         begin processing inbox
         """
-        self.process = Process(target = SupportingActor.listen, args = [self.inbox, self.receive, self.callback, self.timeout, self.handle, self.lock])
+        self.process = Process(target = SupportingActor.listen, args = [self.inbox, self.receive, self.callback, self.timeout, self.handle])
         self.process.start()
+
+class SupportingCast(SupportingActor):
+    def __init__(self, num = 1, **kwargs):
+        SupportingActor.__init__(self, **kwargs)
+        self.num = num
+        self._error_lock = Lock()                           # While the error lock is held, message reception in SupportingCast._listen is blocked
+        self._error_pipe_in, self._error_pipe_out = Pipe()  # Errors from individual actors are sent through the error pipe so they are caught by the director process.
+        self._message_received_flag = Value('i', 0)         # This flag gets toggled to 1 when individual actors receive messages, which causes the director to reset the timeout alarm
+
+    def _cast_actor(self):
+        return Process(target = SupportingCast.listen, args = [self.inbox, self.receive, self._message_received_flag, self._error_lock, self._error_pipe_in])
+
+    def _cast_actors(self):
+        return {i : self._cast_actor() for i in xrange(self.num)}
+
+    @staticmethod
+    def listen(inbox, receive, message_received_flag, error_lock, error_pipe_in):
+        """
+        listens for incoming messages
+        """
+        try:
+            SupportingCast._listen(inbox, receive, message_received_flag, error_lock)
+        except Exception as exc:
+            error_lock.acquire()
+            error_pipe_in.send(exc)
+
+    @staticmethod
+    def _listen(inbox, receive, message_received_flag, error_lock):
+        running = True
+        while running:
+            error_lock.acquire() # Blocks message reception while there is an error
+            error_lock.release()
+            message = inbox.get()
+            message_received_flag.value = 1
+            receive(message)
+
+    @staticmethod
+    def direct(callback, timeout, handle, error_pipe_out, message_received_flag):
+        try:
+            SupportingCast._direct(timeout, error_pipe_out, message_received_flag)
+        except Timeout:
+            callback()
+        except Exception as exc:
+            handle(exc)
+
+    @staticmethod
+    def _direct(timeout, error_pipe_out, message_received_flag):
+        use_timeout = True if type(timeout) == int else False           # We use a timeout if timeout is an integer, otherwise not
+        if use_timeout: signal.signal(signal.SIGALRM, _raise_timeout)   # _raise_timeout is called when alarm goes off
+        running = True
+        while running:
+            if error_pipe_out.poll():                   # If there is an exception in the error pipe,
+                exc = error_pipe_out.recv()             # we catch it,
+                raise exc                               # and raise it
+            if use_timeout:
+                if message_received_flag.value == 1: 
+                    signal.alarm(timeout)               # Set/Reset alarm if some process received a message
+                    message_received_flag.value = 0     # Then toggle the flag back to zero
+
+    def __call__(self):
+        self.actors = self._cast_actors()
+        for actor in self.actors.values(): actor.start()
+        self.director = Process(target = SupportingCast.direct, args = [self.callback, self.timeout, self.handle, self._error_pipe_out, self._message_received_flag])
+        self.director.start()
 
 def _raise_timeout(signum, frame):
     raise Timeout
-
-def _do_nothing(*args, **kwargs):
-    pass
-
-def print_square(message):
-    print "%s squared is: %s" %(message, message**2)
-
-square_printer = SupportingActor(receive = print_square, timeout = 25)
-
-square_printer()
-
-for i in xrange(1,11):
-    square_printer.inbox.put(i)
-
-square_printer.process.is_alive()
-
-class Cast(SupportingActor):
-    def __init__(self, supporting_actor, num = 1):
-        self.actors = _cast_actors(supporting_actor, num)
-
-    def _cast_actors(self, supporting_actor, num):
-        actors = {}
-        for i in xrange(num):
-            actors[i] = SupportingActor()
-            
-        pass
-
-    @staticmethod
-    def direct(listen, inbox, receive, callback, timeout, handle):
-        pass
-
-        
-
-
-    @staticmethod
-    def _direct(timeout, processes):
-        use_timeout = True if type(timeout) == int else False
-        if use_timeout: signal.signal(signal.SIGALRM, _raise_timeout) # _raise_timeout is called when alarm goes off
-        running = True
-        while running:
-            if use_timeout: signal.alarm(timeout) # Set/Reset alarm
-            message = inbox.get()
-            if use_timeout: signal.alarm(0) # Alarm turned off when processing message
-            receive(message)
-
-    def __call__(self):
-        self.processes = []
-
-        self.director = Process(target = Cast.direct, args = [self.listen, self.inbox, self.receive, self.callback, self.timeout, self.handle])
-        self.director.start()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

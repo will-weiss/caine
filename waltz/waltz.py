6be99aab24a17da778deb14cca6b4af1a9d6c72e
will-cognius <will@cognius.com>
2014-08-18 23:00:34 -0400

    unclean

--- waltz/waltz.py
+++ waltz/waltz.py
@@ -1,4 +1,4 @@
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Lock
 import signal
 import time
 from gevent.timeout import Timeout
@@ -9,8 +9,9 @@ class SupportingActor(object):
     """
     def __init__(self, timeout = 10, **kwargs):
         self.inbox = Queue()
+        self.lock = Lock()
         self.timeout = timeout
-        for nm, val in kwargs.iteritems(): setattr(self, nm, val) 
+        for nm, val in kwargs.iteritems(): setattr(self, nm, val)
 
     @staticmethod
     def receive(message):
@@ -27,26 +28,83 @@ class SupportingActor(object):
         print "No more messages in inbox."
 
     @staticmethod
-    def handle():
+    def handle(lock):
         """
         method to call when operation fails
         """
+        lock.acquire()
         print "Operation failed."
 
     @staticmethod
-    def listen(inbox, receive, callback, timeout, handle):
+    def listen(inbox, receive, callback, timeout, handle, lock):
         """
         listens for incoming messages
         """
         try:
-            SupportingActor._listen(inbox, timeout, receive)
+            SupportingActor._listen(inbox, timeout, receive, lock)
         except Timeout:
             callback()
         except not Timeout:
-            handle()
+            handle(lock)
 
     @staticmethod
-    def _listen(inbox, timeout, receive):
+    def _listen(inbox, timeout, receive, lock):
+        use_timeout = True if type(timeout) == int else False
+        if use_timeout: signal.signal(signal.SIGALRM, _raise_timeout) # _raise_timeout is called when alarm goes off
+        running = True
+        while running:
+            if use_timeout: signal.alarm(timeout) # Set/Reset alarm
+            lock.acquire()
+            lock.release()
+            message = inbox.get()
+            if use_timeout: signal.alarm(0) # Alarm turned off when processing message
+            receive(message)
+
+    def __call__(self):
+        """
+        begin processing inbox
+        """
+        self.process = Process(target = SupportingActor.listen, args = [self.inbox, self.receive, self.callback, self.timeout, self.handle, self.lock])
+        self.process.start()
+
+def _raise_timeout(signum, frame):
+    raise Timeout
+
+def _do_nothing(*args, **kwargs):
+    pass
+
+def print_square(message):
+    print "%s squared is: %s" %(message, message**2)
+
+square_printer = SupportingActor(receive = print_square, timeout = 25)
+
+square_printer()
+
+for i in xrange(1,11):
+    square_printer.inbox.put(i)
+
+square_printer.process.is_alive()
+
+class Cast(SupportingActor):
+    def __init__(self, supporting_actor, num = 1):
+        self.actors = _cast_actors(supporting_actor, num)
+
+    def _cast_actors(self, supporting_actor, num):
+        actors = {}
+        for i in xrange(num):
+            actors[i] = SupportingActor()
+            
+        pass
+
+    @staticmethod
+    def direct(listen, inbox, receive, callback, timeout, handle):
+        pass
+
+        
+
+
+    @staticmethod
+    def _direct(timeout, processes):
         use_timeout = True if type(timeout) == int else False
         if use_timeout: signal.signal(signal.SIGALRM, _raise_timeout) # _raise_timeout is called when alarm goes off
         running = True
@@ -55,13 +113,31 @@ class SupportingActor(object):
             message = inbox.get()
             if use_timeout: signal.alarm(0) # Alarm turned off when processing message
             receive(message)
-    
-    def __call__(self):
-        """
-        begin processing inbox
-        """
-        self.process = Process(target = SupportingActor.listen, args = [self.inbox, self.receive, self.callback, self.timeout, self.handle])
-        self.process.start()
 
-def _raise_timeout(signum, frame):
-    raise Timeout
+    def __call__(self):
+        self.processes = []
+
+        self.director = Process(target = Cast.direct, args = [self.listen, self.inbox, self.receive, self.callback, self.timeout, self.handle])
+        self.director.start()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

3a8ba9ee004fb2ccef07a422b0dc25326fc35c49
will-weiss <will.weiss1230@gmail.com>
2014-09-16 12:59:29 -0400

    v0.1.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='caine',
-    version='0.1.8',
+    version='0.1.9',
     author='Will Weiss',
     author_email='will.weiss1230@gmail.com',
     packages=['caine'],
